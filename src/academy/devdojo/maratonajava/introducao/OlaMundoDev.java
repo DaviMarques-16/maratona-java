301163e5c14b57ef399b788a82e1df80b1752b3c
Davi Marques <142530449+DaviMarques-16@users.noreply.github.com>
2025-05-04 13:36:41 -0300

    Primeiro commit

---
+++ src/academy/devdojo/maratonajava/introducao/OlaMundoDev.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ *  Isto é um comentário <b>javadoc</b>
+ */
+
+public class OlaMundoDev {
+    // Isto é um comentário da linha
+
+    /*
+        Isto é um comentário de múltiplas linhas
+     */
+    public static void main(String[] args){
+        System.out.println("One slip and falling back into the hedge maze");
+    }
+}
